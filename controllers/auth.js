6cb18d1355f3861b115ffc8947f940119bb596ef
Julian Usma Giraldo <jeumag86@gmail.com>
2022-02-03 13:24:19 -0500

    Apple signin start

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,7 @@
+const fs = require("fs");
 const { response } = require("express");
+const AppleAuth = require("apple-auth");
+const jwt = require("jsonwebtoken");
 const { validateGoogleIdToken } = require("../helpers/google_verify_token");
 
 const googleAuth = async (req, res = response) => {
@@ -27,5 +30,58 @@ const googleAuth = async (req, res = response) => {
     googleUser,
   });
 };
+//Callback para Apple
+const callBackApple = async (request, response) => {
+  const redirect = `intent://callback?${new URLSearchParams(
+    request.body
+  ).toString()}#Intent;package=${
+    process.env.ANDROID_PACKAGE_IDENTIFIER
+  };scheme=signinwithapple;end`;
 
-module.exports = { googleAuth };
+  console.log(`Redirecting to ${redirect}`);
+
+  response.redirect(307, redirect);
+};
+
+//signin para apple
+const signInWithApple = async (request, response) => {
+  const auth = new AppleAuth(
+    {
+      // use the bundle ID as client ID for native apps, else use the service ID for web-auth flows
+      // https://forums.developer.apple.com/thread/118135
+      client_id:
+        request.query.useBundleId === "true"
+          ? process.env.BUNDLE_ID
+          : process.env.SERVICE_ID,
+      team_id: process.env.TEAM_ID,
+      redirect_uri:
+        "https://apple-google-signin-serv.herokuapp.com/callbacks/sign_in_with_apple", // does not matter here, as this is already the callback that verifies the token after the redirection
+      key_id: process.env.KEY_ID,
+    },
+    fs.readFileSync("../keys/AuthKey_MKN4A7QNX5.p8").toString(),
+    "text"
+  );
+
+  console.log(request.query);
+
+  const accessToken = await auth.accessToken(request.query.code);
+
+  const idToken = jwt.decode(accessToken.id_token);
+
+  const userID = idToken.sub;
+
+  console.log(idToken);
+
+  // `userEmail` and `userName` will only be provided for the initial authorization with your app
+  const userEmail = idToken.email;
+  const userName = `${request.query.firstName} ${request.query.lastName}`;
+
+  // 👷🏻‍♀️ TODO: Use the values provided create a new session for the user in your system
+  const sessionID = `NEW SESSION ID for ${userID} / ${userEmail} / ${userName}`;
+
+  console.log(`sessionID = ${sessionID}`);
+
+  response.json({ sessionId: sessionID });
+};
+
+module.exports = { googleAuth, callBackApple, signInWithApple };
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,10 @@
 const { Router } = require("express");
 const { append } = require("express/lib/response");
-const { googleAuth } = require("../controllers/auth");
+const {
+  googleAuth,
+  callBackApple,
+  signInWithApple,
+} = require("../controllers/auth");
 
 const router = Router();
 
@@ -8,5 +12,13 @@ router.post(
   "/google", //google controller
   googleAuth
 );
+// The callback route used for Android, which will send the callback parameters from Apple into the Android app.
+// This is done using a deeplink, which will cause the Chrome Custom Tab to be dismissed and providing the parameters from Apple back to the app.
+router.post("/callbacks/sign_in_with_apple", callBackApple);
+
+// Endpoint for the app to login or register with the `code` obtained during Sign in with Apple
+//
+// Use this endpoint to exchange the code (which must be validated with Apple within 5 minutes) for a session in your system
+router.post("/sign_in_with_apple", signInWithApple);
 
 module.exports = router;
