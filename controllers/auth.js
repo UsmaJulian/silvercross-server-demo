fb5a96619845f39f2015a0b639b4f18734233e0d
Julian Usma Giraldo <jeumag86@gmail.com>
2022-02-04 15:13:38 -0500

    Commit try catch.

--- controllers/auth.js
+++ controllers/auth.js
@@ -32,56 +32,64 @@ const googleAuth = async (req, res = response) => {
 };
 //Callback para Apple
 const callBackApple = async (request, response) => {
-  const redirect = `intent://callback?${new URLSearchParams(
-    request.body
-  ).toString()}#Intent;package=${
-    process.env.ANDROID_PACKAGE_IDENTIFIER
-  };scheme=signinwithapple;end`;
+  try {
+    const redirect = `intent://callback?${new URLSearchParams(
+      request.body
+    ).toString()}#Intent;package=${
+      process.env.ANDROID_PACKAGE_IDENTIFIER
+    };scheme=signinwithapple;end`;
 
-  console.log(`Redirecting to ${redirect}`);
+    console.log(`Redirecting to ${redirect}`);
 
-  response.redirect(307, redirect);
+    response.redirect(307, redirect);
+  } catch (error) {
+    console.log(`Callback error: ${error}`);
+  }
 };
 
 //signin para apple
 const signInWithApple = async (request, response) => {
-  const auth = new AppleAuth(
-    {
-      // use the bundle ID as client ID for native apps, else use the service ID for web-auth flows
-      // https://forums.developer.apple.com/thread/118135
-      client_id:
-        request.query.useBundleId === "true"
-          ? process.env.BUNDLE_ID
-          : process.env.SERVICE_ID,
-      team_id: process.env.TEAM_ID,
-      redirect_uri:
-        "https://apple-google-signin-serv.herokuapp.com/callbacks/sign_in_with_apple", // does not matter here, as this is already the callback that verifies the token after the redirection
-      key_id: process.env.KEY_ID,
-    },
-    process.env.KEY_CONTENTS.replace(/\|/g, "\n"),
-    "text"
-  );
+  try {
+    const auth = new AppleAuth(
+      {
+        // use the bundle ID as client ID for native apps, else use the service ID for web-auth flows
+        // https://forums.developer.apple.com/thread/118135
+        client_id:
+          request.query.useBundleId === "true"
+            ? process.env.BUNDLE_ID
+            : process.env.SERVICE_ID,
+        team_id: process.env.TEAM_ID,
+        redirect_uri:
+          "https://apple-google-signin-serv.herokuapp.com/callbacks/sign_in_with_apple", // does not matter here, as this is already the callback that verifies the token after the redirection
+        key_id: process.env.KEY_ID,
+      },
+      process.env.KEY_CONTENTS.replace(/\|/g, "\n"),
+      "text"
+    );
 
-  console.log(request.query);
+    console.log(request.query);
 
-  const accessToken = await auth.accessToken(request.query.code);
+    const accessToken = await auth.accessToken(request.query.code);
 
-  const idToken = jwt.decode(accessToken.id_token);
+    const idToken = jwt.decode(accessToken.id_token);
 
-  const userID = idToken.sub;
+    const userID = idToken.sub;
 
-  console.log(idToken);
+    console.log(idToken);
 
-  // `userEmail` and `userName` will only be provided for the initial authorization with your app
-  const userEmail = idToken.email;
-  const userName = `${request.query.firstName} ${request.query.lastName}`;
+    // `userEmail` and `userName` will only be provided for the initial authorization with your app
+    const userEmail = idToken.email;
+    const userName = `${request.query.firstName} ${request.query.lastName}`;
 
-  // 👷🏻‍♀️ TODO: Use the values provided create a new session for the user in your system
-  const sessionID = `NEW SESSION ID for ${userID} / ${userEmail} / ${userName}`;
+    // 👷🏻‍♀️ TODO: Use the values provided create a new session for the user in your system
+    const sessionID = `NEW SESSION ID for ${userID} / ${userEmail} / ${userName}`;
 
-  console.log(`sessionID = ${sessionID}`);
+    console.log(`sessionID = ${sessionID}`);
 
-  response.json({ sessionId: sessionID });
+    response.json({ sessionId: sessionID });
+  } catch (error) {
+    console.log(`signInWithApple error: ${error}`);
+  }
 };
 
 module.exports = { googleAuth, callBackApple, signInWithApple };
