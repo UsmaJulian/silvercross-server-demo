a05859ed101fbaf949150df62edc3e77af5e64ae
Julian Usma Giraldo <jeumag86@gmail.com>
2022-02-04 13:09:20 -0500

    Commit view, public .

--- index.js
+++ index.js
@@ -6,9 +6,16 @@ const app = express();
 
 //parsear el body para mostrar la respuesta
 app.use(bodyParser.urlencoded({ extended: true }));
+// make all the files in 'public' available
+// https://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
 
 //informar sobre la disposición de nuevas rutas al server
 //RUTAS
+// https://expressjs.com/en/starter/basic-routing.html
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/views/index.html");
+});
 app.use("/", require("./routes/auth"));
 
 app.listen(process.env.PORT || 3000, () => {
